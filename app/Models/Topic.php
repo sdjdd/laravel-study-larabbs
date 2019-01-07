642590007b404bce28ee4ae28d53058f260da0f5
sdjdd <sdjddrt@gmail.com>
2019-01-07 09:03:50 +0000

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,8 +2,21 @@
 
 namespace App\Models;
 
-class Topic extends Model {
+class Topic extends Model
+{
 
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    protected $fillable = [
+        'title', 'body', 'user_id', 'category_id', 'reply_count',
+        'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'
+    ];
     
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
