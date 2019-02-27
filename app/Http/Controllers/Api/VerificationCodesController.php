18eaf79f989f11b810768329825a0e3d66942ee9
sdjdd <sdjddrt@gmail.com>
2019-02-27 09:56:31 +0000

    Add captcha

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -9,22 +9,36 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request)
     {
-        $phone = $request->phone;
-        if (app()->environment('production')) {
-            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
-        } else {
-            $code = '1234';
-        }
-        $key = 'verificationCode_'.str_random(15);
+        $captchaData = \Cache::get($request->captcha_key);
 
+        if (!$captchaData) {
+            return $this->response->error('图片验证码已失效', 422);
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        $phone = $captchaData['phone'];
+
+        if (!app()->environment('production')) {
+            $code = '1234';
+        } else {
+            // 生成4位随机数，左侧补0
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+        }
+
+        $key = 'verificationCode_'.str_random(15);
         $expiredAt = now()->addMinutes(10);
-        \Cache::put($key, [
-            'phone' => $phone,
-            'code'  => $code,
-        ], $expiredAt);
+        // 缓存验证码 10分钟过期。
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
 
         return $this->response->array([
-            'key'        => $key,
+            'key' => $key,
             'expired_at' => $expiredAt->toDateTimeString(),
         ])->setStatusCode(201);
     }
