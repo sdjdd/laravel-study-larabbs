38445ef3be4451fb73cd4720ce500d20f9d7fe1b
sdjdd <sdjddrt@gmail.com>
2019-03-01 01:03:20 +0000

    Topic update API

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,5 +32,9 @@ class AppServiceProvider extends ServiceProvider {
         if (app()->isLocal()) {
             $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
         }
+
+        \API::error(function (\Illuminate\Auth\Access\AuthorizationException $exception) {
+            abort(403, $exception->getMessage());
+        });
     }
 }
